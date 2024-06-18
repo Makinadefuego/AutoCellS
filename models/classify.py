5d13e9d12637152610f305d92d5acbfa68516d50
Makinadefuego <fportillap2100@alumno.ipn.mx>
2024-06-18 10:38:36 -0600

    Cambiando librería 2

--- models/classify.py
+++ models/classify.py
@@ -30,26 +30,28 @@ def clasificar_celula(imagen_path):
     try:
         modelo_clasificacion = load_model(model_path)
         print(f"Modelo cargado correctamente desde {model_path}")
+
+        img_width, img_height = 256, 256
+        """
+        Clasifica una sola imagen de célula.
+
+        Args:
+            imagen_path (str): Ruta a la imagen de la célula.
+
+        Returns:
+            str: Nombre de la clase predicha.
+        """
+        img = image.load_img(imagen_path, target_size=(img_width, img_height))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+
+
+        prediction = modelo_clasificacion.predict(x)
+        class_index = np.argmax(prediction)
+
     except ValueError as e:
         print(f"Error al cargar el modelo: {str(e)}")
-    img_width, img_height = 256, 256
-    """
-    Clasifica una sola imagen de célula.
-
-    Args:
-        imagen_path (str): Ruta a la imagen de la célula.
-
-    Returns:
-        str: Nombre de la clase predicha.
-    """
-    img = image.load_img(imagen_path, target_size=(img_width, img_height))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-
-
-    prediction = modelo_clasificacion.predict(x)
-    class_index = np.argmax(prediction)
-
+    
     class_names = [
         "Interfase",
         "Profase",
